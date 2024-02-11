c5510a1d5043579d0a873bf8d7a019e82b253000
njofredev <nico.jofreandrade@gmail.com>
2024-02-11 16:45:18 -0300

    Add Exercise 1, 2 and 3 into trabajo-grupal

---
+++ trabajo-grupal/ejercicio2.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 2 - Trabajo práctico grupal Python - njofre
+'Ingreso de notas'
+
+Realizar un programa que lea por teclado las 5 notas obtenidas por un alumno 
+(comprendidas entre 0 y 10). A continuación, debe mostrar todas las notas, la nota media,
+la notas más alta que ha sacado y la nota menor
+"""
---
+++ trabajo-grupal/ejercicio3.py
@@ -0,0 +1,8 @@
+"""
+Ejercicio 3 - Trabajo práctico grupal Python - njofre
+'Meses'
+
+Crea un programa que pida un número de dia al usuario y un número de mes e indique 
+el script cuantos días tiene ese mes y el nombre del mes que pidió. Debes usar listas.
+
+"""
