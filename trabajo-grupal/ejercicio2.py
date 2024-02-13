61e17a8c38b2d6f8a530108722706168a7afe45a
njofredev <nico.jofreandrade@gmail.com>
2024-02-13 17:10:02 -0300

    Add ejercicios 1,2,3 trabajo-grupal folder

--- trabajo-grupal/ejercicio2.py
+++ trabajo-grupal/ejercicio2.py
@@ -4,5 +4,28 @@ Ejercicio 2 - Trabajo práctico grupal Python - njofre
 
 Realizar un programa que lea por teclado las 5 notas obtenidas por un alumno 
 (comprendidas entre 0 y 10). A continuación, debe mostrar todas las notas, la nota media,
-la notas más alta que ha sacado y la nota menor
+la notas más alta que ha sacado y la nota menor.
 """
+
+def ingresoNotas():
+    contador = 1
+    notaIngresada = []
+    notaMenor = []
+    notaAlta = []
+    
+    while contador <= 5:
+        notaIngresada.append(int(input(f"Ingrese la nota n° {contador} \n")))
+        contador += 1
+        
+    for c in notaIngresada:
+        print(c)
+    mediana = sorted(notaIngresada)
+    
+    print("Todas las notas ingresadas son: \n")
+    print(mediana)
+
+        
+    
+ingresoNotas()
+
+# Terminar desarrollo quedé en imprimir la mediana de los números ingresados 
