f50576e5eed83515608fc574b8cb304029c4196f
njofredev <nico.jofreandrade@gmail.com>
2024-02-13 13:35:10 -0300

    Add ejercicio1.py

--- ejercicios/ejercicio1.py
+++ ejercicios/ejercicio1.py
@@ -1,3 +1,9 @@
 """
-Trabajo clase 13-02-24
+Trabajo clase 13-02-24 - njofre
+'Actividad: Dar solución a un problema con pseudo-código'
+
+Plantear un algoritmo de la vida real y solucionarlo con pseudocódigo
 """
+
+
+
