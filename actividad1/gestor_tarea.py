795e3bf594d2c61c4f180b6e358334a0b6f11705
njofredev <nico.jofreandrade@gmail.com>
2024-02-14 10:08:27 -0300

    Add gestor_tareas.py changes

--- actividad1/gestor_tarea.py
+++ actividad1/gestor_tarea.py
@@ -13,3 +13,26 @@ Nicolás Jofré Andrade
 - Cómo se se completan y se guardan en un archivo de texto llamado:
     - tareas.txt
 """
+# Se definen las 4 funciones pedidas
+def agregar_tarea():
+    print("Se ingresa a la función de agregar_tarea()")
+
+def mostrar_tareas():
+    print("Se ingresa a la función mostrar_tareas()")
+
+def completar_tarea():
+    print ("Se ingresa a la función completar_tarea()")
+
+def guardar_tareas():
+    print("Se ingresa la función guardar_tareas()")
+
+# Inicio de programa 
+
+
+
+
+# Inicialización de las 4 funciones
+"""agregar_tarea()
+mostrar_tareas()
+completar_tarea()
+guardar_tareas()"""
