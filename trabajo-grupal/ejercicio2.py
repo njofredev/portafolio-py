c2a7ac31d781ae3df63d585b06baa386b4afc78b
njofredev <nico.jofreandrade@gmail.com>
2024-02-15 13:07:08 -0300

    Fix ejercicio1.py

--- trabajo-grupal/ejercicio2.py
+++ trabajo-grupal/ejercicio2.py
@@ -6,3 +6,6 @@ Realizar un programa que lea por teclado las 5 notas obtenidas por un alumno
 (comprendidas entre 0 y 10). A continuación, debe mostrar todas las notas, la nota media,
 la notas más alta que ha sacado y la nota menor
 """
+
+for n in range(5):
+    numIngresado = float(input("Ingrese un número entre 0 y 10"))
