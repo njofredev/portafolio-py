5c882d48e3f590e0b7dddbeed84f0c9493fb0833
njofredev <nico.jofreandrade@gmail.com>
2024-02-17 04:22:59 -0300

    Fix ejercicio2.py comments and syntax

--- trabajo-grupal/ejercicio2.py
+++ trabajo-grupal/ejercicio2.py
@@ -6,21 +6,22 @@ Realizar un programa que lea por teclado las 5 notas obtenidas por un alumno
 (comprendidas entre 0 y 10). A continuación, se debe mostrar todas las notas, la nota media,
 la notas más alta que ha sacado y la nota menor.
 """
-
+print("Bienvenido!")
+print("A continuación, ingrese 5 notas (puede ingresar con decimal)")
 notas = [] # Se inicializa la lista de notas
 
 for n in range(5): # Se recorre las 5 notas por ingresar
-    numIngresado = float(input("Ingrese un número entre 0 y 10 \n"))
+    numIngresado = float(input(f"Ingrese una nota entre 0.0 y 10.0 \n"))
     
     if numIngresado < 0 or numIngresado > 10:
-        print("ERROR: Ingrese un número en 0 y 10 \n")
+        print("ERROR: Ingrese una nota entre 0.0 y 10.0 \n")
     else:
         notas.append(numIngresado)
         print("Ingreso correcto!")    
      
-notaAlta = max(notas)
-notaBaja = min(notas)
-notaMedia = sum(notas) / 5
+notaAlta = max(notas) # Función para saber el máximo de la lista notas[]
+notaBaja = min(notas) # Función para saber el mínimo de la lista notas[]
+notaMedia = sum(notas) / 5 # Función para saber la suma de la lista notas[] y dividirla por la cantidad ingresada (5)
 
 print(f"Todas las notas son: {notas}")    
 print(f"La nota mayor es {notaAlta} y la nota menor es {notaBaja} y la nota promedio es: {notaMedia} ")
