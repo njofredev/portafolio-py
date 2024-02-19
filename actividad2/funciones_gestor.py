6e9bcc94dd56d0f57392f088dfc499a031de3376
njofredev <nico.jofreandrade@gmail.com>
2024-02-19 17:14:23 -0300

    Add gestor_tareas_avanzado and module funciones_gestor.py

---
+++ actividad2/funciones_gestor.py
@@ -0,0 +1,25 @@
+import datetime
+
+# Se definen las funciones mejoradas del gestor de tareas
+def agregar_tareas_con_fecha(lista_tareas, fecha_tarea):
+    cant_tareas = int(input("¿Cuántas tareas quieres agregar?: \n"))
+    
+    for n in range(cant_tareas):
+        tarea = str(input(f"Ingrese la tarea n°{n + 1}: \n"))
+        fecha = str(input("Ingrese la fecha de ingreso (dia/mes/año): \n"))
+        lista_tareas.append(tarea)
+        fecha_tarea.append(fecha)
+    print("Tareas con fecha ingresadas correctamente")
+    
+def mostrar_detalles_tareas(lista_tareas, fecha_tarea):
+    print(f"La cantidad de tareas ingresadas es: {len(lista_tareas)}")
+    # Se recorre la lista lista_tareas[] y fecha_tarea[] con los datos ingresados
+    for n in range(len(lista_tareas)):
+        print(f"La tarea n°{n + 1} es: {lista_tareas[n]} y fue ingresada el: {fecha_tarea[n]}")
+    
+def completar_tareas():
+    print("Aquí se pueden completar las tareas ingresadas")
+    
+def guardar_tareas():
+    print("Aquí se guardan las tareas")
+
--- actividad2/gestor_tareas_avanzado.py
+++ actividad2/gestor_tareas_avanzado.py
@@ -7,3 +7,35 @@ Nicolás Jofré Andrade
 - Implementa un sistema mejorado de gestión de tares que utilice 
 diferentes tipos de datos y sentencias básicas de Python.
 """
+# Import de funciones dentro de módulo funciones_gestor.py
+import funciones_gestor
+import datetime
+
+lista_tareas = []
+fecha_tarea = []
+
+# Se llaman las funciones de funciones_gestor.py
+while True:
+    print("\n *** Gestor de tareas Avanzado con Python *** \n")
+    print("1. Agregar tareas con fecha de ingreso")
+    print("2. Mostrar tareas con detalles")
+    print("3. Guarde las tareas en el archivo tareas.txt")
+    print("4. Ingrese 0 para salir \n")
+    opcion_menu = int(input("Ingrese una opción: \n"))
+
+    if opcion_menu == 0:
+        print("¡Gracias por participar en el gestor de tareas, vuelve pronto!")
+        break
+    elif opcion_menu == 1:
+        funciones_gestor.agregar_tareas_con_fecha(lista_tareas, fecha_tarea)  
+    elif opcion_menu == 2:
+        funciones_gestor.mostrar_detalles_tareas(lista_tareas, fecha_tarea)
+    elif opcion_menu == 3:
+        funciones_gestor.completar_tareas()
+    elif opcion_menu == 0:
+        print("¡Gracias por participar en el gestor de tareas!")
+    else:
+        print("Ingrese un valor entre 0 y 3")
+        
+
+# Sum: Terminé agregar tareas con fechas y mostrarlas con funciones
