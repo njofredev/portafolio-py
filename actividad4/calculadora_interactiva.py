3d800279fd2439b6ecdabd8c681ec819aa838f3d
njofredev <nico.jofreandrade@gmail.com>
2024-02-21 11:32:47 -0300

    Add ejercicio4 solution

--- actividad4/calculadora_interactiva.py
+++ actividad4/calculadora_interactiva.py
@@ -8,3 +8,45 @@ Nicolás Jofré Andrade
 matemáticas de manera interactiva. El programa debe utilizar ciclos para 
 que los usuarios puedan realizar múltiples operaciones sin reiniciar el programa.
 """
+from funciones_calculadora import *
+
+while True:
+    # Menú básico para la calculadora
+    print("\n*** Calculadora básica con Python *** \n")
+    print("Instrucciones: Ingrese una de las opciones a realizar. \n")
+    print("1. Suma de dos números")
+    print("2. Resta de dos números")
+    print("3. Multiplicación de dos números")
+    print("4. División de dos números")
+    print("5. Salir del programa \n")
+
+    text_primer_numero = "Ingrese el primero número: "
+    text_segundo_numero = "Ingrese el segundo número: "
+
+    opcion = int(input("Ingrese una opción: \n"))
+
+    if opcion == 1:
+        num1 = int(input(text_primer_numero))
+        num2 = int(input(text_segundo_numero))
+        resultado_suma = sumarNumeros(num1,num2)
+        print(f"\nEl resultado de la suma es: {resultado_suma}")
+    elif opcion == 2:
+        num1 = int(input(text_primer_numero))
+        num2 = int(input(text_segundo_numero))
+        resultado_resta = restarNumeros(num1,num2)
+        print(f"\nEl resultado de la resta es: {resultado_resta}")
+    elif opcion == 3:
+        num1 = int(input(text_primer_numero))
+        num2 = int(input(text_primer_numero))
+        resultado_multiplicacion = multiplicarNumeros(num1,num2)
+        print(f"\nEl resultado de la multiplicación es: {resultado_multiplicacion}")
+    elif opcion == 4:
+        num1 = int(input(text_primer_numero))
+        num2 = int(input(text_segundo_numero))
+        resultado_division = dividirNumeros(num1,num2)
+        print(f"\nEl resultado de la división es: {resultado_division}")
+    elif opcion == 5:
+        print("Gracias por participar en la calculadora básica con Python!")
+        break
+    else:
+        print("Ingrese un valor del menú indicado")
---
+++ actividad4/funciones_calculadora.py
@@ -0,0 +1,13 @@
+# Se definen las funciones de la calculadora básica 
+
+def sumarNumeros(a,b):
+    return a+b
+
+def restarNumeros(a,b):
+    return a-b
+
+def multiplicarNumeros(a,b):
+    return a*b
+
+def dividirNumeros(a,b):
+    return a/b
