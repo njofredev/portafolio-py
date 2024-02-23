35b74da8663ae5c7046d9eec2c8498bb0c08549d
njofredev <nico.jofreandrade@gmail.com>
2024-02-23 19:23:01 -0300

    Changes into actividad3 folder

--- actividad3/evaluacion_rendimiento.py
+++ actividad3/evaluacion_rendimiento.py
@@ -6,17 +6,37 @@ Nicolás Jofré Andrade
 - Crea un archivo llamado 'evaluacion_rendimiento.py'
 - Implementa un programa que evalúe el rendimiento de un estudiante
 según sus calificaciones en diferentes asignaturas.
+- Funciones:
+    - definir_estudiantes()
+    - mostrar_notas()
 """
 from funciones_evaluacion import * 
 
 
-# Definición de estudiantes
+# Atributos iniciales de estudiantes
 lista_notas = []
-nombre_estudiante = ""
-edad_estudiante = 0
-asignatura_estudiante = ""
-cantidad_notas = 0
+nombre_estudiante = []
+edad_estudiante = []
+asignatura_estudiante = []
+cantidad_notas = []
 
-definir_estudiantes(lista_notas, nombre_estudiante, edad_estudiante, asignatura_estudiante, cantidad_notas)
+#Inicio de programa
+while True:
+    print("\n *** Evaluación de rendimiento *** \n")
+    print("1. Ingresar un estudiante")
+    print("2. Detalles de estudiante")
+    print("3. Rendimiento de notas ingresadas")
+    print("4. Salir del programa \n")
+    opcion_menu = int(input("Ingrese una opción: \n"))
 
-comparacionNotas(lista_notas, nombre_estudiante, edad_estudiante, asignatura_estudiante, cantidad_notas)
+    if opcion_menu == 5:
+        print("¡Gracias por participar en el gestor de rendimiento, vuelve pronto!")
+        break
+    elif opcion_menu == 1:
+        definir_estudiantes(lista_notas, nombre_estudiante, edad_estudiante, asignatura_estudiante, cantidad_notas) 
+    elif opcion_menu == 2:
+        mostrar_notas(lista_notas, nombre_estudiante, edad_estudiante, asignatura_estudiante, cantidad_notas)
+    elif opcion_menu == 3:
+        comparacion_notas(lista_notas, nombre_estudiante, asignatura_estudiante)
+    else:
+        print("Ingrese un valor entre 0 y 5")
--- actividad3/funciones_evaluacion.py
+++ actividad3/funciones_evaluacion.py
@@ -1,26 +1,63 @@
 # Funciones de evaluación de rendimiento 
 
 def definir_estudiantes(lista_notas, nombre_estudiante, edad_estudiante, asignatura_estudiante, cantidad_notas):
+    
+    try:
+        print("< Gestión de rendimiento de estudiante >\n")
+        nombre = str(input("Ingrese el nombre del estudiante: \n"))
+        nombre_estudiante.append(nombre)
+        edad = int(input("Ingrese la edad del estudiante: \n"))
+        edad_estudiante.append(edad)
+        # Ingresar diferentes asignaturas
+        asignatura = str(input("Ingrese la asignatura del estudiante: \n"))
+        asignatura_estudiante.append(asignatura)
+        cant_notas = int(input("Ingrese una cantidad de notas a evaluar: \n"))
+        cantidad_notas.append(cant_notas)
+        print(f"A continuación, ingrese las {cant_notas} notas de {nombre}: ")
 
-    print("*** Gestión de rendimiento con Python ***\n")
-    nombre_estudiante = str(input("Ingrese el nombre del estudiante: \n"))
-    edad_estudiante = int(input("Ingrese la edad del estudiante: \n"))
-    asignatura_estudiante = str(input("Ingrese la asignatura del estudiante: \n"))
-    cantidad_notas = int(input("Ingrese una cantidad de notas a evaluar: \n"))
-    print(f"A continuación, ingrese las {cantidad_notas} notas de {asignatura_estudiante}: ")
+        for n in range(cant_notas):
+            nota = float(input(f"Ingrese la nota n°{n + 1}: "))
+            lista_notas.append(nota)
+            print ("Ingreso correcto!")
+            
+        #for m in range() / Cantidad de asignaturas
+    except ValueError:
+        print("ERROR: Ingrese un valor real.")
 
-    for n in range(cantidad_notas):
-        lista_notas = []
-        nota = float(input(f"Ingrese la nota n°{n + 1}: "))
-        lista_notas.append(nota)
-        print ("Ingreso correcto!")
+def mostrar_notas(lista_notas, nombre_estudiante, edad_estudiante, asignatura_estudiante, cantidad_notas):
+    
+    print("< Datos del estudiante: >")
+    print(f"El nombre del usuario es: {nombre_estudiante[0]}")
+    print(f"La edad del estudiante es: {edad_estudiante[0]}")
+    print(f"La asignatura del estudiante es: {asignatura_estudiante[0]}")
+    print(f"La cantidad de notas ingresadas es: {cantidad_notas}")
+    print(f"La lista completa de notas es: {lista_notas}") 
+    
+def comparacion_notas(lista_notas, nombre_estudiante, asignatura_estudiante):
 
-    return lista_notas, nombre_estudiante,edad_estudiante,asignatura_estudiante,cantidad_notas
+    aprobado = True
+    reprobado = False
+    
+    for n in range(len(lista_notas)):
+        promedio = sum(lista_notas) / len(lista_notas)
+    
+    print(f"Las notas son: {lista_notas} \n")    
+    print(f"El promedio de notas es:{promedio} ")    
 
-def comparacionNotas(lista_notas, nombre_estudiante, edad_estudiante, asignatura_estudiante, cantidad_notas):
-    print("Datos del estudiante: ")
-    print(f"La lista completa de notas es: {lista_notas}")
-    print(f"El nombre del usuario es: {nombre_estudiante}")
-    print(f"La edad del estudiante es: {edad_estudiante}")
-    print(f"La asignatura del estudiante es: {asignatura_estudiante}")
-    print(f"La cantidad de notas ingresadas es: {cantidad_notas}")    
+    if promedio >= 4.0:
+        print(f"Tu promedio ({promedio}) de {asignatura_estudiante} -  califica como APROBADO")
+        estado = aprobado
+    elif promedio <= 3.9:
+        print(f"Tu promedio {promedio} califica como REPROBADO")
+        estado = reprobado
+    
+    if estado == True:
+        print(f"¡Felicitaciones {nombre_estudiante[0]}, has aprobado {asignatura_estudiante[0]}:) !")
+    else:
+        print(f"¡Lo sentimos {nombre_estudiante[0]}, has reprobado {asignatura_estudiante[0]} :(!")
+
+"""
+sum: 
+Falta ingresar for para la cantidad de asignaturas - agregar asignaturas
+Todas las otras funciones están listas
+"""
